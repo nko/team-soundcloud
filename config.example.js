8a867f1c7096404deffc60778012592e96d2319b
Sebastian <ohm.sebastian@gmail.com>
2010-08-28 17:55:59 +0200

    maybe generate varnish load like this

--- config.example.js
+++ config.example.js
@@ -3,3 +3,14 @@ var path = require('path')
 exports.root    = path.dirname(__filename)
 
 exports.frontend = { port: 8080 }
+
+// twitter api details + auth credentials
+exports.twitter = { host: 'stream.twitter.com'
+                  , endpoint: '/1/statuses/filter.json?track=bit'
+                  , auth: 'foo' // Base64.encode64(username:password)
+                  }
+
+// host + url that can be used to generate traffic against varnish
+exports.varnish = { host: '127.0.0.1'
+                  , port: 8080
+                  }
--- server.js
+++ server.js
@@ -4,9 +4,17 @@ var http           = require('http')
   , frontend       = new http.Server()
   , frontendStatic = new(require('node-static').Server)('./public')
   , socket         = io.listen(frontend)
+  , url            = require('url')
+/*
+  , twitter        = new(require('./lib/twitter').Twitter)( config.twitter.host
+                                                          , config.twitter.endpoint
+                                                          , config.twitter.auth
+                                                          );
+*/
 
-frontend.addListener('request', function (req, res) {
-  req.addListener('end', function () {
+// handle static file requests + websocket clients
+frontend.on('request', function (req, res) {
+  req.on('end', function () {
     frontendStatic.serve(req, res)
   });
 });
@@ -18,7 +26,6 @@ socket.on('connection', function (client) {
 frontend.listen(config.frontend.port);
 
 // generate events and broadcast to all clients
-
 setInterval(function () {
   var d = new Date();
   socket.broadcast(JSON.stringify({ key:  'date', value: d }));
@@ -28,3 +35,15 @@ setInterval(function () {
   var t = new Date().getMilliseconds();
   socket.broadcast(JSON.stringify({ key: 'counter', value: t }));
 }, 223);
+
+/*
+// generate varnish traffic
+twitter.on('message', function (msg) {
+  var varnish = http.createClient(config.varnish.port, config.varnish.host)
+    , request = varnish.request('GET', '/' + msg, { host : config.varnish.host });
+
+  request.end();
+});
+
+twitter.read();
+*/
