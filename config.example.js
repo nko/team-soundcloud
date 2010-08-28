fab56cbea4e4b160be0939365a0e0e0e6917f5d8
Sebastian <ohm.sebastian@gmail.com>
2010-08-28 14:00:29 +0200

    socket.io backend deps

--- config.example.js
+++ config.example.js
@@ -3,3 +3,5 @@ var path = require('path')
 exports.root    = path.dirname(__filename)
 
 exports.frontend = { port: 8080 }
+
+exports.backend = { port: 8081 }
--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-var http        = require('http')
-  , config      = require('./config')
-  , webroot     = config.webroot
-  , frontend    = new http.Server()
-  , fileServer  = new(require('node-static').Server)('./public')
+var http           = require('http')
+  , config         = require('./config')
+  , io             = require('socket.io')
+  , frontend       = new http.Server()
+  , frontendStatic = new(require('node-static').Server)('./public')
+  , backend        = new http.Server()
+  , socket         = io.listen(backend)
 
 frontend.addListener('request', function (req, res) {
-  req.addListener('end', function() {
-    fileServer.serve(req, res)
-  })
-}).listen(config.frontend.port)
+  req.addListener('end', function () {
+    frontendStatic.serve(req, res)
+  });
+});
+
+frontend.listen(config.frontend.port);
+
+backend.addListener('request', function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello world</h1>');
+  res.end();
+});
+
+backend.listen(config.backend.port);
+
+socket.on('connection', function (client) {
+  client.broadcast({ announcement: client.sessionId + ' connected' });
+});
