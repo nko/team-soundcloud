977555849f92324b2ffc92851f07c01b0e79254d
Jan Berkel <jan.berkel@gmail.com>
2010-08-28 17:14:54 +0200

    non-blocking I/O

--- varnish-ext/main.c
+++ varnish-ext/main.c
@@ -38,6 +38,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <limits.h>
+#include <assert.h>
 
 #include <varnishapi.h>
 
@@ -48,26 +49,47 @@
 #endif
 
 static void
-accumulate(const unsigned char *p)
+dump(const unsigned char *p)
 {
-  printf("%s\n", p);
+  const unsigned char *q;
+  q = p + SHMLOG_DATA;
+  fprintf(stderr, "%s\n", q);
 }
 
- static void
-dump(void)
+int
+handler(void *priv, enum shmlogtag tag, unsigned fd, unsigned len,
+    unsigned spec, const char *ptr)
 {
+  FILE *fo = priv;
+  int type;
+
+  assert(fo != NULL);
+
+  type = (spec & VSL_S_CLIENT) ? 'c' :
+      (spec & VSL_S_BACKEND) ? 'b' : '-';
+
+  fprintf(fo, "%5d %-12s %c %.*s\n", fd, VSL_tags[tag], type, len, ptr);
+  return (0);
 }
- 
+
 static void
 do_once(struct VSL_data *vd)
 {
   unsigned char *p;
-
   while (VSL_NextLog(vd, &p) > 0)
-    accumulate(p);
-  dump();
+    dump(p);
 }
 
+static void 
+log_loop(struct VSL_data *vd)
+{
+  while (VSL_Dispatch(vd, handler, stdout) >= 0) {
+    if (fflush(stdout) != 0) {
+      perror("stdout");
+      break;
+    }
+  }
+}
 
 int
 main(int argc, char **argv)
@@ -93,6 +115,13 @@ main(int argc, char **argv)
     exit (1);
 
   VSL_NonBlocking(vd, 1);
-  do_once(vd);
+
+  for (;;) {
+    log_loop(vd);
+    //fprintf(stderr, "sleeping\n");
+    sleep(0.5);
+  }
+  //do_once(vd);
+
   exit(0);
 }
