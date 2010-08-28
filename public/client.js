5ae115a912205e8918b1c2dea9b5cd345d260298
Rany Keddo <rany@soundcloud.com>
2010-08-28 16:00:09 +0200

    frontend initial

--- public/client.js
+++ public/client.js
@@ -1,1 +1,47 @@
-/* client js goes here */
+// time plugin
+
+(function($) {
+
+  $.dashboardTime = function() {
+    var now = new Date();
+    return now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+  }
+
+  $.fn.timeize = function(selector) {
+    this.html($.dashboardTime());
+    var that = this;
+
+    setInterval(function() { that.html($.dashboardTime()); }, 1000);
+  };
+
+})(jQuery);
+
+// sparkline plugin with google charts
+
+(function($) {
+
+  $.fn.spark = function(selector) {
+  };
+
+})(jQuery);
+
+
+// apply plugins
+
+$(".time").timeize();
+
+// sockets
+
+io.setPath('/Socket.IO/');
+
+if( window.location.toString().match(/localhost/) ) {
+  socket = new io.Socket('localhost');
+}
+else {
+  socket = new io.Socket('team-soundcloud.no.de', {port: 80});
+}
+
+socket.connect();
+socket.on('message', function(data) {
+  $('#messages').html(data);
+});
