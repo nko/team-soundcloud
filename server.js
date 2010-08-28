4b4a69eb2e2262f76dab344e584151d085a5ae48
Sebastian <ohm.sebastian@gmail.com>
2010-08-28 12:00:36 +0200

    shuffle things and load path

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ HTTP_PORT = 8080
 
 var http     = require('http')
   , path     = require('path')
-  , paperboy = require('./vendor/node-paperboy')
-  , webroot  = path.join(path.dirname(__filename), 'public');
+  , root     = path.dirname(__filename)
+  , paperboy = require(path.join(root, 'vendor', 'paperboy'))
+  , webroot  = path.join(root, 'public');
 
 var frontend = http.createServer(function (req, res) {
   paperboy.deliver(webroot, req, res);
---
+++ vendor/paperboy/example/basic.js
@@ -0,0 +1,40 @@
+var
+  sys = require('sys'),
+  path = require('path'),
+  http = require('http'),
+  paperboy = require('../lib/paperboy'),
+
+  PORT = 8003,
+  WEBROOT = path.join(path.dirname(__filename), 'webroot');
+
+http.createServer(function(req, res) {
+  var ip = req.connection.remoteAddress;
+  paperboy
+    .deliver(WEBROOT, req, res)
+    .addHeader('Expires', 300)
+    .addHeader('X-PaperRoute', 'Node')
+    .before(function() {
+      sys.log('Received Request')
+    })
+    .after(function(statCode) {
+      log(statCode, req.url, ip);
+    })
+    .error(function(statCode,msg) {
+      res.writeHead(statCode, {'Content-Type': 'text/plain'});
+      res.write("Error: " + statCode);
+      res.end();
+      log(statCode, req.url, ip, msg);
+    })
+    .otherwise(function(err) {
+      var statCode = 404;
+      res.writeHead(statCode, {'Content-Type': 'text/plain'});
+      log(statCode, req.url, ip, err);
+    });
+}).listen(PORT);
+
+function log(statCode, url, ip,err) {
+  var logStr = statCode + ' - ' + url + ' - ' + ip
+  if (err)
+    logStr += ' - ' + err;
+  sys.log(logStr);
+}
